8222a2c4933d0a3447c08e0f32307920bf77f6fb
Rob Bovill <rbovill@lsst.org>
2018-02-08 14:00:42 -0700

    Added the tcsAOS and tcsWEP CSCs to xml_common.py.

--- scripts/xml_common.py
+++ scripts/xml_common.py
@@ -8,7 +8,7 @@ import re
 # Variables
 # =========
 """Defines the array of Commandable SAL Components, or CSCs."""
-subsystems = ['archiver', 'calibrationElectrometer', 'camera', 'catchuparchiver', 'dmHeaderService', 'dome', 'domeADB', 'domeAPS', 'domeLouvers', 'domeLWS', 'domeMONCS', 'domeTHCS', 'eec', 'environment', 'hexapod', 'm1m3', 'm2ms', 'MTMount', 'ocs', 'processingcluster', 'rotator', 'scheduler', 'sequencer', 'summitFacility', 'tcs']
+subsystems = ['archiver', 'calibrationElectrometer', 'camera', 'catchuparchiver', 'dmHeaderService', 'dome', 'domeADB', 'domeAPS', 'domeLouvers', 'domeLWS', 'domeMONCS', 'domeTHCS', 'eec', 'environment', 'hexapod', 'm1m3', 'm2ms', 'MTMount', 'ocs', 'processingcluster', 'rotator', 'scheduler', 'sequencer', 'summitFacility', 'tcs', 'tcsAOS', 'tcsWEP']
 
 
 # =========
@@ -41,6 +41,10 @@ def CapitalizeSubsystem( subsystem ):
 		return "TCS"
 	elif subsystem == "ocs":
 		return "OCS"
+	elif subsystem == "tcsAOS":
+		return "TCSAOS"
+	elif subsystem == "tcsWEP":
+		return "TCSWEP"
 	else:
 		return subsystem.capitalize()
 
