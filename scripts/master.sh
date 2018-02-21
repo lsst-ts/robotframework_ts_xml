202084c06debaa1074cb7de79a69b067e1e476e9
Rob Bovill <rbovill@lsst.org>
2018-02-21 15:34:05 -0700

    Added XML_ReservedWords.py to the master.sh.

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,2 +1,2 @@
-python XML_Count.py; python XML_Definitions.py  python XML_NoSpaces.py; python XML_Size.py; python XML_Version.py; python XML_XSD.py
+python XML_Count.py; python XML_Definitions.py  python XML_NoSpaces.py; python XML_Size.py; python XML_Version.py; python XML_XSD.py; python XML_ReservedWords.py
 
