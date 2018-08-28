8fef1909352f502cbb9302dc371014a5934f60f0
Rob Bovill <rbovill@lsst.org>
2018-08-28 11:05:22 -0700

    Added whitespace to skip tag assignments in XML_Size.py.

--- scripts/XML_Size.py
+++ scripts/XML_Size.py
@@ -50,21 +50,21 @@ for subsystem in xml_common.subsystems:
 			# Mark test cases with Jira tickets
 			byte_skipped=col_skipped=""
 			if subsystem == "sedSpectrometer" and topic.decode("utf-8") == "sedSpectrometer_logevent_measuredSpectrum":
-				col_skipped="TSS-2987"
+				col_skipped="	TSS-2987"
 			elif subsystem == "sedSpectrometer" and topic.decode("utf-8") == "sedSpectrometer_logevent_internalCommand":
-				col_skipped="TSS-2988"
+				col_skipped="	TSS-2988"
 			elif subsystem == "m1m3" and topic.decode("utf-8") == "m1m3_command_RunMirrorForceProfile":
-				col_skipped="TSS-2989"
+				col_skipped="	TSS-2989"
 			elif subsystem == "m1m3" and topic.decode("utf-8") == "m1m3_logevent_ForceActuatorInfo":
-				col_skipped="TSS-2990"
+				col_skipped="	TSS-2990"
 			elif subsystem == "m1m3" and topic.decode("utf-8") == "m1m3_logevent_ForceSetpointWarning":
-				col_skipped="TSS-2991"
+				col_skipped="	TSS-2991"
 			elif subsystem == "m1m3" and topic.decode("utf-8") == "m1m3_logevent_ForceActuatorWarning":
-				col_skipped="TSS-2992"
+				col_skipped="	TSS-2992"
 			elif subsystem == "atcs" and topic.decode("utf-8") == "atcs_logevent_InternalCommand":
-				col_skipped="TSS-2994"
+				col_skipped="	TSS-2994"
 			elif subsystem == "tcs" and topic.decode("utf-8") == "tcs_logevent_InternalCommand":
-				col_skipped="TSS-2561"
+				col_skipped="	TSS-2561"
 			else:
 				byte_skipped=""
 				col_skipped=""
