04cdd16d83a23f9e7d448738610c2ba07e123098
Rob Bovill <rbovill@lsst.org>
2018-08-31 16:58:31 -0700

    Fixed a typo in XML_Generic.py.

--- scripts/XML_Generic.py
+++ scripts/XML_Generic.py
@@ -96,7 +96,7 @@ for subsystem in xml_common.subsystems:
 		enum_skipped="    TSS-2984"
 	elif subsystem == "calibrationElectrometer":
 		enum_skipped="    TSS-2997"
-	elif subsystem == "Spectrometer":
+	elif subsystem == "Spectrograph":
 		enum_skipped="    TSS-3009"
 	else:
 		skipped=""
